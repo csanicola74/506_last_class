2e9d39c1422e7c88043c8961b39657f5fc40077d
csanicola74 <109254340+csanicola74@users.noreply.github.com>
2022-08-10 19:04:31 -0400

    first commit

---
+++ python1.py
@@ -0,0 +1,2 @@
+# the .env file is greyed out because that houses files to be restricted access for whatever reason
+# so for hiding secret files that could include passwords or are just larger, you can put it in the name in the env file
